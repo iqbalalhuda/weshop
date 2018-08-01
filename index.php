bb4cda0e9ff2b2138573bb57b1404f69809382e4
iqbalalhuda <38303303+iqbalalhuda@users.noreply.github.com>
2018-08-01 21:51:16 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,44 +1,100 @@
 <?php
-// memanggil library FPDF
-require('fpdf.php');
-// intance object dan memberikan pengaturan halaman PDF
-$pdf = new FPDF('l','mm','A5');
-// membuat halaman baru
-$pdf->AddPage();
-// setting jenis font yang akan digunakan
-$pdf->SetFont('Arial','B',16);
-// mencetak string 
-$pdf->Cell(190,7,'DAFTAR PEMESANAN BARANG',0,1,'C');
-$pdf->SetFont('Arial','B',12);
-$pdf->Cell(190,7,'ALRIBAL SHOP',0,1,'C');
 
-// Memberikan space kebawah agar tidak terlalu rapat
-$pdf->Cell(10,4,'',0,1);
+	session_start();
 
-$pdf->SetFont('Arial','B',10);
-$pdf->Cell(25,6,'ID PESANAN',1,0);
-$pdf->Cell(20,6,'ID KOTA',1,0);
-$pdf->Cell(20,6,'USER ID',1,0);
-$pdf->Cell(50,6,'NAMA PENERIMA',1,0);
-$pdf->Cell(27,6,'NOMOR HP',1,0);
-$pdf->Cell(85,6,'ALAMAT',1,0);
-$pdf->Cell(25,6,'TANGGAL PEMESANAN',1,1);
+	include_once("function/koneksi.php");
+	include_once("function/helper.php");
 
-$pdf->SetFont('Arial','',10);
-
-include 'koneksi.php';
-$pesanan = mysqli_query($connect, "select * from pesanan");
-while ($row = mysqli_fetch_array($pesanan)){
-	$pdf->Cell(25,6,$row['pesanan_id'],1,0);
-	$pdf->Cell(20,6,$row['kota_id'],1,0);
-	$pdf->Cell(20,6,$row['user_id'],1,0);
-	$pdf->Cell(50,6,$row['nama_penerima'],1,0);
-	$pdf->Cell(27,6,$row['nomor_telepon'],1,0);
-	$pdf->Cell(85,6,$row['alamat'],1,0);
-	$pdf->Cell(25,6,$row['tanggal_pemesanan'],1,1);
+	$page = isset($_GET['page']) ? $_GET['page'] : false;
+	$kategori_id = isset($_GET['kategori_id']) ? $_GET['kategori_id'] : false;
 	
-   
-}
+	$user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : false;
+	$nama = isset($_SESSION['nama']) ? $_SESSION['nama'] : false;
+	$level = isset($_SESSION['level']) ? $_SESSION['level'] : false;
+	$keranjang = isset($_SESSION['keranjang']) ? $_SESSION['keranjang'] : array();
+	$totalBarang = count($keranjang);
 
-$pdf->Output();
 ?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>weshop | barang-barang elektronik</title>
+		
+		<link href="<?php echo BASE_URL."css/style.css"; ?>" type="text/css" rel="stylesheet" />
+		<link href="<?php echo BASE_URL."css/banner.css"; ?>" type="text/css" rel="stylesheet" />
+		
+		<script src="<?php echo BASE_URL."js/jquery-3.1.1.min.js"; ?>"></script>
+		<script src="<?php echo BASE_URL."js/Slides-SlidesJS-3/source/jquery.slides.min.js"; ?>"></script>
+		
+		<script>
+		$(function() {
+			$('#slides').slidesjs({
+				height: 350,
+				play: { auto : true,
+					    interval : 3000
+					  },
+				navigation : false
+			});
+		});
+		</script>		
+		
+	</head>
+
+	<body>
+	
+		<div id="container">
+			<div id="header">
+				<a href="<?php echo BASE_URL."index.php"; ?>">
+					<img src="<?php echo BASE_URL."images/logo.png"; ?>" />
+				</a>
+				
+				<div id="menu">
+					<div id="user">
+						<?php
+							if($user_id){
+								echo "Hi <b>$nama</b>,
+									  <a href='".BASE_URL."index.php?page=my_profile&module=pesanan&action=list'>My Profile</a>
+									  <a href='".BASE_URL."logout.php'>Logout</a>";
+							}else{
+								echo "<a href='".BASE_URL."index.php?page=login'>Login</a>
+									 <a href='".BASE_URL."index.php?page=register'>Register</a>";
+							}
+						?>
+					</div>
+					
+					<a href="<?php echo BASE_URL."index.php?page=keranjang"; ?>" id="button-keranjang">
+						<img src="<?php echo BASE_URL."images/cart.png"; ?>" /> 
+						<?php
+							if($totalBarang != 0){
+								echo "<span class='total-barang'>$totalBarang</span>";
+							}
+						?>
+					</a>
+				</div>
+			</div>	
+
+			<div id="content">
+				
+				<?php
+					$filename = "$page.php";
+					
+					if(file_exists($filename)){
+						include_once($filename);
+					}else{
+						include_once("main.php");
+					}
+				?>
+			
+			</div>
+			
+			<div id="footer">
+				<p>copyright weshop 2016</p>
+			</div>
+			
+		</div>
+	
+	</body>
+	
+</html>
